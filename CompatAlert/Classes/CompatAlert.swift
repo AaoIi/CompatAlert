6b76590c69b9013bc6ae01e29b6b7fc1e44b87f6
AaoIi <user@users-Mac-mini-2.local>
2016-01-05 16:29:04 +0200

    Alert for IOS 7+ written in Swift

---
+++ CompatAlert/Classes/CompatAlert.swift
@@ -0,0 +1,104 @@
+//
+//  CompatAlert.swift
+//  CompatAlert
+//
+//  Created by AaoIi on 1/5/16.
+//  Copyright © 2016 Saad Albasha. All rights reserved.
+//
+
+import UIKit
+
+class CompatAlert: NSObject,UIAlertViewDelegate {
+
+    // create singleton instance
+    static let instance = CompatAlert()
+    
+    private override init() {
+        super.init()
+    }
+    
+    
+    // showAlert will check if the iOS version is 8 and up or 7 and show AlertController for iOS 8 and up,Also if its iOS 7 it will show AlertView..
+    
+    func showAlert(title:String,message:String,buttonTitleYes:String,buttonTitleCancel:String,controller:UIViewController,tag:Int){
+        
+        if #available(iOS 8, *) {
+            // IOS 8 and up
+            
+            if buttonTitleYes == "" {
+                // without action only cancel
+                let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+                alert.addAction(UIAlertAction(title: buttonTitleCancel, style: UIAlertActionStyle.Cancel, handler: nil))
+                controller.presentViewController(alert, animated: true, completion: nil)
+                
+            }else {
+                // with action
+                let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+                alert.addAction(UIAlertAction(title: buttonTitleCancel, style: UIAlertActionStyle.Cancel, handler: nil))
+                alert.addAction(UIAlertAction(title: buttonTitleYes, style: UIAlertActionStyle.Default, handler: checkTagAndReturnAction(tag)))
+                controller.presentViewController(alert, animated: true, completion: nil)
+                
+            }
+ 
+            
+        }else {
+            //IOS 7
+            if buttonTitleYes == "" {
+                //without action only cancel
+                let alert = UIAlertView(title: title, message: message, delegate: self, cancelButtonTitle: buttonTitleCancel)
+                alert.show()
+                
+                
+            }else {
+                // with action
+                let alert = UIAlertView(title: title, message: message, delegate: self, cancelButtonTitle: buttonTitleCancel, otherButtonTitles: buttonTitleYes)
+                alert.tag = tag
+                alert.show()
+            }
+            
+        }
+        
+    }
+    
+    // IOS 8 and up alert action
+    
+    @available(iOS 8.0, *)
+    func checkTagAndReturnAction(tag: Int) -> ((UIAlertAction) -> (Void)){
+        
+        // activited only when buttonTitleYes not empty
+        switch (tag) {
+        case 0:
+            return { (UIAlertAction) -> Void in
+                print("No internet Connection-AlertController")
+            }
+            
+        default:
+            return { (UIAlertAction) -> Void in
+                print("default")
+            }
+        }
+
+    }
+    
+    // IOS 7 and up action
+    
+    func alertView(alertView: UIAlertView, clickedButtonAtIndex buttonIndex: Int) {
+        
+        // activited only when buttonTitleYes not empty
+        if buttonIndex == 1 {
+            
+            switch (alertView.tag) {
+            case 0:
+                print("No internet Connection-AlertView")
+                break;
+                
+            default:
+                break;
+            }
+        }
+        
+        
+    }
+
+    
+}
--- CompatAlert/ViewController.swift
+++ CompatAlert/ViewController.swift
@@ -2,24 +2,38 @@
 //  ViewController.swift
 //  CompatAlert
 //
-//  Created by user on 1/5/16.
+//  Created by AaoIi on 1/5/16.
 //  Copyright © 2016 Saad Albasha. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+
+    }
+
+    @IBAction func alertWithoutAction(sender: AnyObject) {
+
+        CompatAlert.instance.showAlert("Sorry", message: "This is just a simple message !", buttonTitleYes: "", buttonTitleCancel: "OK", controller: self, tag: 0)
+        
+    }
+    
+    @IBAction func alertWithAction(sender: AnyObject) {
+        
+        CompatAlert.instance.showAlert("Sorry", message: "Please check your internet connection!", buttonTitleYes: "Retry", buttonTitleCancel: "Dismiss", controller: self, tag: 0)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
